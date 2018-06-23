4aedd89effcac8b522354711fb1acb4c0f0da7cb
robertHer <totolero769@gmail.com>
2018-06-22 17:54:44 -0700

    managing recipe in recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit , Output, EventEmitter} from '@angular/core';
 
 import {Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'recipe-list',
@@ -8,20 +9,14 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    {
-      name: 'A new recipe',
-      description: 'test recipe',
-      imagePath: 'https://media-cdn.tripadvisor.com/media/photo-s/0b/05/86/57/grilled-norwegian-salmon.jpg'
-    },
-    new Recipe('A Test Recipe',
-      'This is a simple test.',
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/pizza-sticks-sl-1000.jpg?itok=3cqmo41n')
-  ];
+  recipes: Recipe[] = [];
 @Output() recipeSelected = new EventEmitter<Recipe>();
-  constructor() { }
+  constructor(private recipeService: RecipeService) { 
+    
+  }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
   onRecipeSelected(recipe: Recipe) {
     // pass recipe to recipes component
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,17 @@
+import { Recipe } from "./recipe.model";
 
 export class RecipeService{
+private recipes:Recipe[] = [
+    {
+      name: 'A new recipe',
+      description: 'test recipe',
+      imagePath: 'https://media-cdn.tripadvisor.com/media/photo-s/0b/05/86/57/grilled-norwegian-salmon.jpg'
+    },
+    new Recipe('A Test Recipe',
+      'This is a simple test.',
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/pizza-sticks-sl-1000.jpg?itok=3cqmo41n')];
 
+      getRecipes():Recipe[] {
+          return this.recipes.slice();//returns exact copy of original array  
+      }
 }
